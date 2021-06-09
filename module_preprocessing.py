f0bbf26fe4cbb91384e0babb070d2e431314d547
Ananda Bayu <48423130+anandabayuf@users.noreply.github.com>
2021-06-09 19:45:50 +0700

    Add files via upload

---
+++ module_preprocessing.py
@@ -0,0 +1,13 @@
+# membuat modul preprocessing text untuk luaran WYSIWYG
+# dengan menggunakan library bs4 atau BeautifulSoup4
+# BeautifulSoup doc : https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+# sumber : https://stackoverflow.com/questions/9662346/python-code-to-remove-html-tags-from-a-string
+
+# prerequisite :
+# install bs4 to your python library using pip command 'pip install bs4'
+
+from bs4 import BeautifulSoup
+
+def cleanHTMLTag(raw_html):
+    cleantext = BeautifulSoup(raw_html, "lxml").text
+    return cleantext
